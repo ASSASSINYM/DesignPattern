bfd0c2528e3f559b60cc0fc2d3fca502f9859caa
wupeng <823105162@qq.com>
2019-10-25 07:33:30 +0800

    建造者模式添加注释

--- DesignPatternDemo/Pattern/Builder/BuilderMain.m
+++ DesignPatternDemo/Pattern/Builder/BuilderMain.m
@@ -8,6 +8,14 @@
 
 #import "BuilderMain.h"
 #import "Director.h"
+
+/*
+ 1.按不同牌子组装各种类型的车子
+ 奔驰、宝马;启动、停止、喇叭、引擎
+ 
+ 2.组装各种品牌手机:https://blog.csdn.net/gdutxiaoxu/article/details/52124383
+ 手机品牌:索尼，三星，小米;分辨率，CPU，摄像
+ */
 @implementation BuilderMain
 - (void)main{
     Director * director = [Director new];
